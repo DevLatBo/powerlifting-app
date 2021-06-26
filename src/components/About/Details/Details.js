10320b8440b189b801255a43871d15e9a80a9c19
05K4R1N <oskr.cbba2010@gmail.com>
2021-06-26 17:11:27 -0400

    [M] About page with styled component

--- src/components/About/Details/Details.js
+++ src/components/About/Details/Details.js
@@ -1,122 +1,117 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import {makeStyles} from '@material-ui/core/styles';
+import styled from 'styled-components';
 
 import SquatImage from '../../../assets/images/Squat.jpg';
 import BenchPressImage from '../../../assets/images/BenchPress.jpeg';
 import DeadliftImage from '../../../assets/images/Deadlift.jpg';
 
-const useStyles = makeStyles((theme) => ({
-    sectionHeading: {
-        textAlign: 'center',
-	    marginBottom: '80px',
-        width: '100%',
-        '& h2': {
-            fontSize: '36px',
-            fontWeight: '600',
-            color: '#1E1E1E'
-        },
-        '& em': {
-            fontStyle: 'normal',
-	        color: '#FF0000'
-        },
-        '& span': {
-            display: 'block',
-            marginTop: '15px',
-            textTransform: 'uppercase',
-            fontSize: '15px',
-            color: '#666',
-            letterSpacing: '1px'
-        }
-    },
-    details: {
-        backgroundColor: '#F7F7F7',
-        marginTop: '140px',
-        marginBottom: '-140px',
-        padding: '120px 0px',
-    },
-    detailItem: {
-        '& img': {
-            width: '90%',
-            height: '275px',
-	        overflow: 'hidden',
-            display: 'block',
-            margin: '0 auto'
-        },
-        [theme.breakpoints.up('xs')]: {
-            marginBottom: '30px'
-        },
-        [theme.breakpoints.up('sm')]: {
-            marginBottom: '0px'
-        },
-    },
-    downContent: {
-        backgroundColor: '#FFF',
-        padding: '30px',
-        '& h4': {
-            fontSize: '20px',
-            fontWeight: '700',
-            letterSpacing: '0.25px',
-            marginBottom: '10px'
-        },
-        '& span': {
-            color: '#FF0000',
-            fontWeight: '600',
-            fontSize: '14px',
-            display: 'block',
-            marginBottom: '15px'
-        },
-        '& p': {
-            marginBottom: '20px'
-        }
+const DetailsContainer = styled.div`
+    background-color: #F7F7F7;
+    margin-top: 140px;
+    margin-bottom: -140px;
+    padding: 120px 0px;
+`;
+const SectionHeading = styled.div`
+    text-align: center;
+    margin-bottom: 80px;
+    width: 100%;
+    & h2 {
+        font-size: 36px;
+        font-weight: 600;
+        color: #1E1E1E;
     }
-}));
+    & em {
+        font-style: normal;
+        color: #FF0000;
+    }
+    & span {
+        display: block;
+        margin-top: 15px;
+        text-transform: uppercase;
+        font-size: 15px;
+        color: #666;
+        letter-spacing: 1px;
+    }
+`;
+const DownContent = styled.div`
+    background-color: #FFF;
+    padding: 30px;
+    & h4 {
+        font-size: 20px;
+        font-weight: 700;
+        letter-spacing: 0.25px;
+        margin-bottom: 10px;
+    }
+    & span {
+        color: #FF0000;
+        font-weight: 600;
+        font-size: 14px;
+        display: block;
+        margin-bottom: 15px;
+    },
+    & p {
+        margin-bottom: 20px;
+    }
+`;
+const DetailItem = styled.div`
+    margin-bottom: 30px;
+    & img {
+        width: 90%;
+        height: 275px;
+        overflow: hidden;
+        display: block;
+        margin: 0 auto;
+    }
+    @media (max-width: 767px) {
+        margin-bottom: 0px;
+    }
+`;
+
 const Details = (props) => {
-    const classes = useStyles();
     return (
-        <div className={classes.details}>
+        <DetailsContainer>
             <Grid container>
-                
-                <div className={classes.sectionHeading}>
+                <SectionHeading>
                     <h2>Movimientos del <em>Powerlifting</em></h2>
                     <span>Existen 3 movimientos dentro de esta disciplina</span>
-                </div>
+                </SectionHeading>
                 <Grid item md={4}>
-                    <div className={classes.detailItem}>
+                    <DetailItem>
                         <img src={SquatImage} alt=""/>
-                        <div className={classes.downContent}>
+                        <DownContent>
                             <h4>Sentadilla</h4>
                             <span>Piernas</span>
                             <p>Es un ejercicio, por lo general, dominante de rodilla. 
                                 Esto quiere decir que la ejecución de la misma depende más de una flexión de rodilla que de una flexión de cadera</p>
-                        </div>
-                    </div>
+                        </DownContent>
+                    </DetailItem>
                 </Grid>
                 <Grid item md={4}>
-                    <div className={classes.detailItem}>
+                    <DetailItem>
                         <img src={DeadliftImage} alt=""/>
-                        <div className={classes.downContent}>
+                        <DownContent>
                             <h4>Peso Muerto</h4>
                             <span>Espalda/Lumbar</span>
                             <p>Es un gran ejercicio para trabajar las piernas y glúteos al completo además de otros grupos musculares como el abdomen, 
                                 espalda baja e incluso brazos. Se levanta la barra desde el suelo hasta la cintura.</p>
-                        </div>
-                    </div>
+                        </DownContent>
+                    </DetailItem>
                 </Grid>
                 <Grid item md={4}>
-                    <div className={classes.detailItem}>
+                    <DetailItem>
                         <img src={BenchPressImage} alt=""/>
-                        <div className={classes.downContent}>
+                        <DownContent>
                             <h4>Bench Press</h4>
                             <span>Pectoral</span>
                             <p>Es un ejercicio compuesto que entrena los pectorales (músculos del pecho), los tríceps y los deltoides frontales (hombros). 
                             Es un gran potenciador de la masa muscular de la parte superior del cuerpo, e incluso un importante ejercicio para las mujeres 
                             que quieran entrenar su fuerza de empuje.</p>
-                        </div>
-                    </div>
+                        </DownContent>
+                    </DetailItem>
                 </Grid>
             </Grid>
-        </div>
+        </DetailsContainer>
     );
 }
 
