b88ec168edaa47615867aec49f14a2f4d311286e
05K4R1N <oskr.cbba2010@gmail.com>
2021-06-27 11:00:47 -0400

    [M] Channging to styled components

--- src/components/About/Information/Information.js
+++ src/components/About/Information/Information.js
@@ -1,81 +1,77 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import {makeStyles} from '@material-ui/core/styles';
+import styled from 'styled-components';
 
-import PowerlifterImage from '../../../assets/images/Powerlifter.jpg';
-
-const useStyles = makeStyles((theme) => ({
-    moreInfo: {
-        marginTop: '140px',
-    },
-    leftImage: {
-        '& img':{
-            width: '100%',
-            overflow: 'hidden'
-        }    
-    },
-    moreInfoContent: {
-        backgroundColor: '#F7F7F7',
-    },
-    rightContent: {
-        '& span': {
-            textTransform: 'uppercase',
-            fontSize: '15px',
-            color: '#666',
-            letterSpacing: '1px',
-            marginBottom: '10px',
-            display: 'block'
-        },
-        '& h2': {
-            fontSize: '36px',
-            fontWeight: '600',
-            marginBottom: '35px'
-        },
-        '& em': {
-            fontStyle: 'normal',
-            color: '#FF0000'
-        },
-        '& p': {
-            marginBottom: '30px'
-        },
-        [theme.breakpoints.up('xs')]: {
-            padding: '30px',
-            marginRight: '0px',
-            marginBottom: '30px'
-        },
-        [theme.breakpoints.up('sm')]: {
-            padding: '45px 45px 45px 30px',
-            marginRight: '30px',
-            marginBottom: '0px'
-        },
+const Definition = styled.div`
+    padding: 45px 45px 45px 30px;
+    margin-right: 30px;
+    margin-bottom: 0px;
+    @media (max-width: 767px) {
+        padding: 30px;
+        margin-right: 0px;
+        margin-bottom: 30px;
     }
-}));
+    & span {
+        text-transform: uppercase;
+        font-size: 15px;
+        color: #666;
+        letter-spacing: 1px;
+        margin-bottom: 10px;
+        display: block;
+    }
+    & h2 {
+        font-size: 36px;
+        font-weight: 600;
+        margin-bottom: 35px;
+    }
+    & em {
+        font-style: normal;
+        color: #FF0000;
+    }
+    & p {
+        margin-bottom: 30px;
+        font-size:14px;
+    }
+`;
+const Reference = styled.div`
+    & img {
+        width: 100%;
+        overflow: hidden;
+    }
+    @media (min-width: 1280px) {
+        margin-top: 2rem;
+    }
+    @media (min-width: 960px) and (max-width: 1280px) {
+        margin-top: 15rem;
+    }  
+`;
+const Info = styled.div`
+    margin-top: 140px;
+    background-color: #F7F7F7;
+`;
 const Information = (props) => {
-    const classes = useStyles();
     return(
-        <div className={[classes.moreInfo, classes.aboutInfo].join(" ")}>
-            <div className={classes.moreInfoContent}>
-                <Grid container>
-                        <Grid item md={6}>
-                            <div className={classes.rightContent}>
-                                <span>Disciplina de fuerza</span>
-                                <h2>Conoce mas sobre <em>Powerlifting</em></h2>
-                                <p>Es una disciplina de fuerza, pues consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
-                                    Para realizar esta disciplina, el atleta debe tener un entrenamiento físico y mental compuesto por los siguientes ejercicios: <strong>Sentadillas, press de banca y peso muerto.</strong>
-                                <br/><br/>El objetivo de cada uno de estos ejercicios es levantar el máximo peso posible mientras se ejecutan, 
-                                    con un recorrido completo y según una normativa establecida. En competiciones profesionales, 
-                                    el levantamiento válido más pesado es el que cuenta para cada prueba y el ganador es el que consiga mejor puntuación 
-                                    por cada uno de los movimientos y por la suma de los tres.</p>
-                            </div>
-                        </Grid>
-                        <Grid item md={6}>
-                            <div className={classes.leftImage}>
-                                <img src={PowerlifterImage} alt=""/>
-                            </div>
-                        </Grid>
+        <Info>
+            <Grid container>
+                <Grid item md={6}>
+                    <Definition>
+                        <span>Disciplina de fuerza</span>
+                        <h2>Conoce mas sobre <em>Powerlifting</em></h2>
+                        <p>Es una disciplina de fuerza, pues consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
+                            Para realizar esta disciplina, el atleta debe tener un entrenamiento físico y mental compuesto por los siguientes ejercicios: <strong>Sentadillas, press de banca y peso muerto.</strong>
+                        <br/><br/>El objetivo de cada uno de estos ejercicios es levantar el máximo peso posible mientras se ejecutan, 
+                            con un recorrido completo y según una normativa establecida. En competiciones profesionales, 
+                            el levantamiento válido más pesado es el que cuenta para cada prueba y el ganador es el que consiga mejor puntuación 
+                            por cada uno de los movimientos y por la suma de los tres.</p>
+                    </Definition>
                 </Grid>
-            </div>
-        </div>
+                <Grid item md={6}>
+                    <Reference>
+                        <img src={`assets/images/Powerlifter.jpg`} alt=""/>
+                    </Reference>
+                </Grid>
+            </Grid>
+        </Info>
     );
 }
 
