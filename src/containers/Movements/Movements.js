a20e01cf146c942da9df7e31e9c2f03fe9fbc0ec
05K4R1N <oskr.cbba2010@gmail.com>
2021-08-09 07:26:37 -0400

    [M] MOdifying styles for movements button section

--- src/containers/Movements/Movements.js
+++ src/containers/Movements/Movements.js
@@ -1,64 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-import {makeStyles} from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles((theme) => ({
-    Movements: {
-        marginTop: "50px"
-    },
-    MovGrid: {
-        display: "inherit"
-    },
-    Movement: {
-        background: "linear-gradient(90deg, rgba(70,24,24,1) 0%, rgba(255,6,6,1) 75%)",
-        boxShadow: "0 5px 0 darkred",
-        color: "white",
-        borderRadius: "35px",
-        margin: "20px auto",
-        textAlign: "center",
-        padding: "50px 0px",
-        position: "relative",
-        textDecoration: "none",
-        textTransform: "uppercase",
-        display:"inline-block",
-        cursor:"pointer",
-        width:'75%',
-        fontSize: '23px',
-        fontWeight: 'bold',
-        '&:active':{
-            boxShadow: 'none',
-            top: '5px'        
-        }
+const MovOptions = styled(Grid)`
+    margin-top: 30px;
+    & .movGrid {
+        display: inherit;
     }
-}));
+    & .movGrid .movement {
+        background: linear-gradient(90deg, rgba(70,24,24,1) 0%, rgba(255,6,6,1) 75%);
+        box-shadow: 0 5px 0 darkred;
+        color: white;
+        border-radius: 35px;
+        margin: 20px auto;
+        text-align: center;
+        padding: 50px 0px;
+        position: relative;
+        text-decoration: none;
+        text-transform: uppercase;
+        display: inline-block;
+        cursor: pointer;
+        width: 75%;
+        font-size: 23px;
+        font-weight: bold;
+    }
+    & .movGrid .movement:active {
+        box-shadow: none;
+        top: 5px;
+    }
+`;
+
 const Movements = (props) => {
-    const classes = useStyles();
     return (
-        <Grid container 
+        <MovOptions container 
             direction="row"
             justify="center"
-            alignItems="center"
-            className={classes.Movements}>
-            <Grid xs={12} md={12} className={classes.MovGrid} item={true}>
-                <Link className={classes.Movement} 
+            alignItems="center">
+            <Grid xs={12} md={12} className="movGrid" item={true}>
+                <Link className="movement"
                     to= "/movements/bench-press">
                         Bench Press
                 </Link>
             </Grid>
-            <Grid xs={12} md={12} className={classes.MovGrid} item={true}>
-                <Link className={classes.Movement} 
+            <Grid xs={12} md={12} className="movGrid" item={true}>
+                <Link className="movement"
                     to="/movements/squat">
                         Squat
                 </Link>
             </Grid>
-            <Grid xs={12} md={12} className={classes.MovGrid} item={true}>
-                <Link className={classes.Movement} 
+            <Grid xs={12} md={12} className="movGrid" item={true}>
+                <Link className="movement"
                     to= "/movements/deadlift">
                         Deadlift
                 </Link>
             </Grid>
-        </Grid>
+        </MovOptions>
     );
 }
 
