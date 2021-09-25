36db8a997b511730986c98fca2810d72825bdbf4
05K4R1N <oskr.cbba2010@gmail.com>
2021-09-25 07:38:08 -0400

    [D] Removing old code

--- src/components/Movements/MovementItem/MovementItemForm/MovementItemForm.js
+++ src/components/Movements/MovementItem/MovementItemForm/MovementItemForm.js
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import {connect} from 'react-redux';
 
 import Button from '../../../UI/Button/Button';
 import Input from '../../../UI/Input/Input';
-import axios from '../../../../axios-records';
 import {updateObject, checkValidity} from '../../../../shared/utility';
-import * as actions from '../../../../store/actions/index';
-import withErrorHandler from '../../../../hoc/withErrorHandler/withErrorHandler';
 
 const FormWeight = styled.div`
     margin: 20px auto;
@@ -50,7 +46,7 @@ const MovementItemForm = (props) => {
             required: true,
         }
     });
-    const [ formIsValid, setFormIsValid] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
 
     const inputChangeHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(weightForm[inputIdentifier], {
@@ -78,7 +74,7 @@ const MovementItemForm = (props) => {
         const liftData = {
             weight: formData
         };
-        props.onLiftWeight(liftData);
+        props.onAddLift(liftData);
     };
 
     const formElementsArray = [];
@@ -114,10 +110,5 @@ const MovementItemForm = (props) => {
         </FormWeight>
     );
 }
-/*const mapDispatchToProps = dispatch => {
-    return {
-        onLiftWeight: (liftData) => dispatch(actions.addLift(liftData))
-    }
-}*/
 
 export default MovementItemForm;
