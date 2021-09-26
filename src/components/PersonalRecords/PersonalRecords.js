8c5a35bda55404ae86e210e199d86adcffe1c5e9
05K4R1N <oskr.cbba2010@gmail.com>
2021-09-25 21:14:20 -0400

    [A] Starting with PR section

--- src/components/PersonalRecords/PersonalRecords.js
+++ src/components/PersonalRecords/PersonalRecords.js
@@ -1,7 +1,133 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 const PersonalRecords = (props) => {
+    const rows = [
+        {movement: 'Deadlift', lift: 95},
+        {movement: 'Bench Press', lift: 42.5},
+        {movement: 'Squat', lift: 100},
+        {movement: 'Deadlift', lift: 140.5},
+        {movement: 'Squat', lift: 70},
+    ];
     return (
-        <h1>Personal Records</h1>
+        <div>
+            <h1>Personal Records</h1>
+            <Grid container 
+                direction="row"
+                justify="center"
+                alignItems="center">
+                <Grid item xs={12} md={4}>
+                    <Box sx={{ minWidth: 275 }}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Box sx={{ minWidth: 275 }}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Box sx={{ minWidth: 275 }}>
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    benevolent
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </Grid>
+            </Grid>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Movimiento</TableCell>
+                            <TableCell align="right">Peso (kg)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.movement}
+                            </TableCell>
+                            <TableCell align="right">{row.lift}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     );
 }
 
