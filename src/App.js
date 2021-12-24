09522eecff8f37cab6ea1f086faac94b3d7539f7
05K4R1N <oskr.cbba2010@gmail.com>
2021-12-24 15:05:44 -0400

    [D] Removing about section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './components/Home/Home';
 import Layout from './hoc/Layout/Layout';
 import Movements from './components/Movements/Movements';
 import MovementItem from './components/Movements/Movement/MovementItemForm/MovementItem';
-import About from './components/About/About';
 import PersonalRecords from './components/PersonalRecords/PersonalRecords';
 import LiftProvider from './store/LiftProvider';
 
@@ -13,7 +12,6 @@ function App() {
   let routes = (
       <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/> 
           <Route path="/prs" component={PersonalRecords}/>
           <Route path="/movements" exact component={Movements}/>
           <Route path="/movements/:movement" component={MovementItem}/>
--- src/components/Home/Information/Definition.js
+++ src/components/Home/Information/Definition.js
@@ -33,13 +33,13 @@ const Definition = (props) => {
                 <Grid item md={12}>
                     <StyledDefinition>
                         <h2>¿Qué es el <em>Powerlifting</em>?</h2>
-                        <p>Es una disciplina de fuerza, pues consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
+                        <p>Es una disciplina de fuerza, que consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
                             Para realizar esta disciplina, el atleta debe tener un entrenamiento físico y mental compuesto 
                             por los siguientes ejercicios: <strong>Sentadillas, press de banca y peso muerto.</strong>
-                        <br/><br/>El objetivo de cada uno de estos ejercicios es levantar el máximo peso posible mientras se ejecutan, 
-                            con un recorrido completo y según una normativa establecida. En competiciones profesionales, 
+                        <br/><br/>El objetivo de cada uno de estos ejercicios es levantar el máximo peso posible durante su ejecución 
+                            siempre y cuando se tenga una postura correcta y sea completa. En competiciones profesionales, 
                             el levantamiento válido más pesado es el que cuenta para cada prueba y el ganador es el que consiga mejor puntuación 
-                            por cada uno de los movimientos y por la suma de los tres.</p>
+                            por cada uno de los movimientos y por la suma de los tres, ya sea dentro de su categoría o sea absoluto.</p>
                     </StyledDefinition>
                 </Grid>
             </Grid>
