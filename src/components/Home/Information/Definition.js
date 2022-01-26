3f52bd939b273ff37268f4832f6157b1d1eeace4
05K4R1N <oskr.cbba2010@gmail.com>
2022-01-25 22:21:21 -0400

    [M] Modifying home sections

--- src/components/Home/Information/Definition.js
+++ src/components/Home/Information/Definition.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import Section from '../../UI/Section/Section';
+import { StyledInformation } from '../../UI/Block/block-styling';
 
-const StyledDefinition = styled.div`
-    & h2 {
-        font-size: 2.25rem;
-        font-weight: 600;
-        margin-bottom: 35px;
-    }
-    & p {
-        font-size: 1.25rem;
-        margin-bottom: 30px;
-    }
-`;
 const Definition = (props) => {
     return(
-        <Section type="definition">
-            <StyledDefinition>
-                <h2>¿Qué es el Powerlifting?</h2>
-                <p>Es una disciplina de fuerza, que consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
-                    Para realizar esta disciplina, el atleta debe tener un entrenamiento físico y mental compuesto 
-                    por los siguientes ejercicios: <strong>Sentadillas, press de banca y peso muerto.</strong></p>
-            </StyledDefinition>
-        </Section>
+        <StyledInformation backgroundColor="#FF0000">
+            <h2>¿Qué es el Powerlifting?</h2>
+            <p>Es una disciplina de fuerza, que consiste en levantar el mayor peso posible en tres ejercicios diferentes. 
+                Para realizar esta disciplina, el atleta debe tener un entrenamiento físico y mental compuesto 
+                por los siguientes ejercicios: <strong>Sentadillas, press de banca y peso muerto.</strong></p>
+        </StyledInformation>
     );
 }
 
--- src/components/Home/Information/Target.js
+++ src/components/Home/Information/Target.js
@@ -1,30 +1,16 @@
-import styled from 'styled-components';
+import { StyledInformation } from '../../UI/Block/block-styling';
 
-import Section from '../../UI/Section/Section';
 
-const StyledTarget = styled.div`
-    & h2 {
-        font-size: 2.25rem;
-        font-weight: 600;
-        margin-bottom: 35px;
-    }
-    & p {
-        font-size: 1.25rem;
-        margin-bottom: 30px;
-    }
-`;
 
 const Target = (props) => {
     return(
-        <Section type="target">
-            <StyledTarget>
-                <h2>¿El Objetivo?</h2>
-                <p>El objetivo de cada uno de los ejercicios es levantar el máximo peso durante su ejecución 
-                        siempre y cuando se tenga una postura correcta y sea completa. En las competiciones 
-                        el levantamiento válido más pesado es el que cuenta para cada prueba y el ganador es el que consiga mejor puntuación 
-                        por cada uno de los movimientos en base a los tres intentos.</p>
-            </StyledTarget>
-        </Section>
+        <StyledInformation backgroundColor="#000000">
+            <h2>¿El Objetivo?</h2>
+            <p>El objetivo de cada uno de los ejercicios es levantar el máximo peso durante su ejecución 
+                    siempre y cuando se tenga una postura correcta y sea completa. En las competiciones 
+                    el levantamiento válido más pesado es el que cuenta para cada prueba y el ganador es el que consiga mejor puntuación 
+                    por cada uno de los movimientos en base a los tres intentos.</p>
+        </StyledInformation>
     );
 }
 
