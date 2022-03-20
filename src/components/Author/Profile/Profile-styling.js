ab3ce7fef55fa209823837b4cd5e0c46fa6aab28
05K4R1N <oskr.cbba2010@gmail.com>
2022-03-19 20:44:24 -0400

    [M] Adjusting css size icon

--- src/components/Author/Profile/Profile-styling.js
+++ src/components/Author/Profile/Profile-styling.js
@@ -6,30 +6,38 @@ import Grid from "@material-ui/core/Grid";
 export const GridStyled = styled(Grid)`
     .photo-profile {
         text-align: center;
-        margin: 40px;
         padding: 10px;
+        margin: 0 40px 0 40px;
     }
     .photo-profile img {
         border-radius: 25px;
         width: 40vw;
-        height: 80vh;
     }
     .description-profile {
         padding: 10px;
-        margin: 40px;
+        margin: 0 40px 0 40px;
         text-align: justify;
     }
-    .description-profile {
+    .description-profile p {
         font-size: 1.3rem;
     }
     .images-profile {
         padding; 10px;
-        margin: 40px;
+        margin: 0 40px 0 40px;
         text-align: center;
     }
     .images-profile img {
-        width: 12vw;
-        height: 25vh;
+        width: 10vw;
         padding: 20px;
+    }
+    @media (min-width: 320px) and (max-width: 480px) {
+        .images-profile img {
+            width: 20vw;
+        }
+    }
+    @media (min-width: 481px) and (max-width: 1024px) {
+        .images-profile img {
+            width: 16vw;
+        }
     }
 `;
